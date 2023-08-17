b8e9cae94437e42f014012179edd3c53f4fa1b17
Christoffer Lind Andersen <57995582+Chri692u@users.noreply.github.com>
2023-08-17 10:22:02 +0200

    example folder

---
+++ Example/app/main.ts
@@ -0,0 +1,8 @@
+// module Main where  --- This is the name of the module
+// const notMain = imports("notMain") --- Returns an object with notMain's scope
+// exports() --- exports nothing
+
+function main() {
+    // notMain.run()
+    return 0
+}
---
+++ Example/src/notMain.ts
@@ -0,0 +1,21 @@
+//module notMain where {
+// imports() --- imports nothing
+// exposed(doTheThing, run) --- exposes the 2 public functions
+
+function doTheThing() {
+    console.log("Doing the thing")
+}
+
+function run() {
+    console.log("running")
+}
+
+function privateLogic() {
+    return "this function is not exposed"
+}
+
+function privateFunction() {
+    return "this function is not exposed"
+}
+
+// } --- module ending
